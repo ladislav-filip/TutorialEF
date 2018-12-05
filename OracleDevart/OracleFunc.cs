24fcbdb38e816eaaa3c67db3fd26400e304d08c7
LF <ladislav.filip@fullsys.cz>
2018-12-05 07:04:48 +0100

    Example wrong LIKE

---
+++ OracleDevart/OracleFunc.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OracleDevart.DAL;
+using System.Linq;
+
+namespace OracleDevart
+{
+    internal static class OracleFunc
+    {
+        internal static void Like()
+        {
+            using (var context = new FisContext())
+            {
+                var ents = context.Set<Entity>();
+
+                // namespace or class "Devart.Data.Oracle.Entity.OracleFunctions" not found
+                //ents = ents.Where(p => Devart.Data.Oracle.Entity.OracleFunctions.Like(p.ENTITA, "HOD"));
+
+                // created bad SQL syntax:
+                // SELECT "p".ENTITA, "p".NAZEV FROM ENTITY "p" WHERE "p".ENTITA LIKE HOD
+                // ORA-00904: "HOD": invalid identifier
+                var data = ents.Where(p => EF.Functions.Like(p.ENTITA, "HOD"));
+
+                data.ToList().ForEach(p => Program.Log(p));
+            }
+        }
+    }
+}
--- OracleDevart/Program.cs
+++ OracleDevart/Program.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Linq;
-using OracleDevart.DAL;
+﻿using System.Diagnostics;
 using static System.Console;
 
 namespace OracleDevart
 {
-class Program
+    class Program
     {
         private static Devart.Data.Oracle.OracleMonitor DbMonitor;
         
@@ -14,35 +12,17 @@ class Program
             DbMonitor = new Devart.Data.Oracle.OracleMonitor();
             DbMonitor.IsActive = true;
             
-            using (var context = new DAL.FisContext())
-            {
-                var config = Devart.Data.Oracle.Entity.Configuration.OracleEntityProviderConfig.Instance;
-                
-                // toto nastavení je platné pro všechny následující Query dotazy
-                config.QueryOptions.CaseInsensitiveLike = true;
-
-                var rkdavs = context.Set<Rkdav>();
-                //var data = rkdavs.ToList();
-                //var data = rkdavs.Where(p => p.RKDAV == "1D                  ").ToList();
-                //var data = rkdavs.Where(p => OracleFunctions.Like(p.RKDAV, "1D  x")).ToList();
-                //var data = rkdavs.Where(p => EF.Functions.Like(p.RKDAV, "'1D   '", "*")).ToList();
-                //var data = rkdavs.Where(p => p.RKDAV.Contains("1d")).ToList();
-                var data = rkdavs.Where(p => p.NAZEV.Contains("pá") || p.RKDAV.Contains("pá")).ToList();
-                WriteLine("---------------------------------------");
-                data.ForEach(p => WriteLine($"'{p.RKDAV}' : {p.NAZEV}"));
-                WriteLine($"Found {data.Count} items.");
-
-                var ent = context.Set<Entity>();
-                var entData = ent.Where(p => p.NAZEV.Contains("maj")).ToList();
-                WriteLine();
-                WriteLine("---------------------------------------");
-                entData.ForEach(p => WriteLine($"'{p.ENTITA}' : {p.NAZEV}"));
-                WriteLine($"Found {data.Count} items.");
-            }
+            // sample wrong LIKE method
+            OracleFunc.Like();
 
             WriteLine();
             WriteLine("Finnish.");
             ReadKey();
+        }
+
+        internal static void Log(object obj)
+        {
+            WriteLine(ObjectDumper.Dump(obj));
         }
 
     }
